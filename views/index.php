a2a9133c26f88d73828ac99b3744ebc7c75629d1
Salman Osmanov <osmanov_s.o@mail.ru>
2020-06-30 23:17:35 +0300

    Made an image as a link

--- views/index.php
+++ views/index.php
@@ -1,8 +1,8 @@
 <?php foreach ($items as $item): ?>
 <table border="2" width="250">
     <tr>
-<td><div> <?php echo $item->title ?> </div></tr>
-<td><img src="<?php echo $item->patch ?>" style="max-width: 200px" </td>
+<td><div> <?php echo $item->title ?> </div></td>
+<td><a href="<?php echo $item->patch ?>"><img src="<?php echo $item->patch ?>"style="max-width: 200px"</a> </td>
     </tr>
 </table>
 <?php endforeach; ?>
