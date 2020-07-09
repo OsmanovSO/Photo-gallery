9ae1a166e2a12421693e18f7161d0b5150875a78
Salman Osmanov <osmanov_s.o@mail.ru>
2020-07-09 22:39:12 +0300

    Added function to delete data from photo gallery

--- views/delitePhoto.php
+++ views/delitePhoto.php
@@ -5,9 +5,9 @@
 <body>
 <h3>Удаление файла с сервера</h3>
 <form method="GET" action="delitePhoto.php" enctype="multipart/form-data">
-    <label for="title"Выберете название фото которое хотите удалить</label>
+    <label for="title"Выберете фото которое хотите удалить</label>
     <input type="text" id="text" name="text"></br>
-    <input type="submit" name="send_messages" value="Загрузить">
+    <input type="submit" name="send_messages" value="Удалить">
 </form>
 </body>
 </html>
--- views/index.php
+++ views/index.php
@@ -1,10 +1,15 @@
+<table border="2" width="250" bordercolor="red">
+
 <?php foreach ($items as $item): ?>
-<table border="2" width="250">
     <tr>
-<td><div> <?php echo $item->title ?> </div></td>
-<td><a href="<?php echo $item->patch ?>"><img src="<?php echo $item->patch ?>"style="max-width: 200px"</a> </td>
-    </tr>
-</table>
+<th align="center"><div> <?php echo $item->id ?> </div></th>
+<th align="center"><div> <?php echo $item->title ?> </div></th>
+<td><a href="<?php echo $item->patch ?>"><img src="<?php echo $item->patch?>"style="max-width: 200px"</a> </td>
+        </tr>
 <?php endforeach; ?>
 
-<?php include __DIR__ . '/addPhoto.php'; ?>
+</table>
+<a href="/addPhoto.php">Добавить фото</a>
+<br>
+<a href="/delitePhoto.php">Удалить фото</a>
+
